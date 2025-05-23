0fd260a1c37988dec30d5e197fe39a40ae1eccfb
Sonali-1615 <sova151624@gmail.com>
2025-05-23 15:41:00 +0530

    Time: 15 ms (40.33%) | Memory: 48.8 MB (52.52%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -4,7 +4,7 @@ class Solution {
         for(int n : nums) {
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        List<Integer> l = new ArrayList<>(map.keySet());;
+        List<Integer> l = new ArrayList<>(map.keySet());
         l.sort((a, b) -> map.get(b) - map.get(a));
         int[] result = new int[k];
         for(int i = 0; i < k; i++) {
