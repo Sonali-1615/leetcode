25b1517fa1a50f6d477b2d316c92c612b2a9d021
Sonali-1615 <sova151624@gmail.com>
2025-05-27 14:42:19 +0530

    Time: 60 ms (80.84%) | Memory: 62.9 MB (31.07%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int[] answer = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!st.isEmpty() && temperatures[i] > temperatures[st.peek()]) {
+                int idx = st.pop();
+                answer[idx] = i - idx;
+            }
+            st.push(i);
+        }
+        return answer;        
+    }
+}
